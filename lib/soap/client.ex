939878f5c06d5466c7885c82652d43b0025fc5c7
Manuel Rubio <manuel@altenwald.com>
2023-03-30 12:59:36 +0200

    output request string as logger debug

--- lib/soap/client.ex
+++ lib/soap/client.ex
@@ -3,6 +3,7 @@ defmodule Soap.Client do
   Let us perform a HTTP request to a SOAP server. If everything goes well
   we will get the correct response and this will be decoded as a map.
   """
+  require Logger
 
   def request(url, %Soap{} = soap) when is_binary(url) do
     url = to_charlist(url)
@@ -16,6 +17,7 @@ defmodule Soap.Client do
       |> Proximal.to_xmlel()
       |> to_string()
 
+    Logger.debug("request: #{req_body}")
     request = {url, req_headers, content_type, req_body}
     resp_head = {'HTTP/1.1', 200, 'OK'}
 
